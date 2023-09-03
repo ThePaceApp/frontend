c1631dbf797d27e74b7cafd2f9dbf0a274980f98
Akinfenwa Taiwo Christopher <taiwoakinfenwa2019@gmail.com>
2023-09-03 17:33:58 +0100

    dashboard

--- creator-dashboard/src/components/sharedComponents/footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './footer.css'
-const Footer = () => {
-  return (
-    <div className='footer'>The Pace App 2021 | All right reserved</div>
-  )
-}
-
-export default Footer
--- creator-dashboard/src/utils/constants.js
+++
@@ -1,4 +0,0 @@
-export const PACEAPP_BASEURL =
-  "https://lime-clever-peacock.cyclic.app/api/v1/creators";
-export const PACEAPP_TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjY0ZDcyNGNiZjU4NzcxZTczMWNmNzY0MCIsImVtYWlsIjoiZGV4bmlzMzBAbWFpbC5jb20iLCJmaXJzdE5hbWUiOiJJc2FhYyIsInJvbGUiOiJjcmVhdG9yIn0sImlhdCI6MTY5MjU1NDE5OCwiZXhwIjoxNjkyNTk3Mzk4fQ.IyBi2-UNJzW0_mMW8dSCq5VNfCdoL1CUy-cEpqb7KQk";
